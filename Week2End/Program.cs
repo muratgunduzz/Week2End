d75228ec458b5c333c57be84b0925c08fd585ae6
murat gunduz <muratgunduz0397@gmail.com>
2025-01-02 16:48:39 +0300

    final result

--- Week2End/Program.cs
+++ Week2End/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Threading.Channels;
 
 class Program
 {
@@ -62,9 +64,9 @@ class Program
 
         //7. Soru
 
-        Console.Write("İlk ismi giriniz:");
+        Console.Write("İlk ismi giriniz: ");
         string firstName = Console.ReadLine();
-        Console.Write("İkinci ismi giriniz:");
+        Console.Write("İkinci ismi giriniz: ");
         string secondName = Console.ReadLine();
 
         string newFirstName = firstName;
@@ -86,6 +88,190 @@ class Program
 
         //10. Soru
 
+        static string BoolToString(bool value)
+        {
+            if (value)
+            {
+                return "true";
+            }
+            else
+            {
+                return "false";
+            }
 
+        }
+
+        Console.WriteLine("Lütfen true yada false değeri giriniz.");
+        bool trueOrFalse = Convert.ToBoolean(Console.ReadLine());
+
+        string result = BoolToString(trueOrFalse);
+        Console.WriteLine("Seçiminiz:" + result);
+
+        //11. Soru
+
+        Console.WriteLine("Lütfen 1. kişinin yaşını giriniz");
+        int age1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Lütfen 2. kişinin yaşını giriniz");
+        int age2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Lütfen 3. kişinin yaşını giriniz");
+        int age3 = Convert.ToInt32(Console.ReadLine());
+        int oldestAge = MaxAge(age1, age2, age3);
+        Console.WriteLine("En yaşlı: " + oldestAge);
+
+
+        static int MaxAge(int age1,int age2,int age3)
+        {
+            int oldest = age1;
+            if(age2 > oldest)
+            {
+                oldest = age2;
+            }
+            if (age3 > oldest) 
+            { 
+                oldest = age3;
+            }
+            return oldest;
+        }
+
+        //12. Soru 
+
+        //Kullanıcıdan sınırsız sayıda sayı alıp , bunlardan en büyüğünü ekrana yazdıran ve aynı zamanda geriye dönen bir metot yazınız.
+
+        static int FindMaxNumber(List<int> numbers)
+        {
+            int max = numbers[0];
+            foreach(int number in numbers)
+            {
+                if(number > max)
+                {
+                    max = number;
+                }
+            }
+            return max;
+        }
+
+        Console.WriteLine("İstediğiniz kadar sayı girebilirsiniz. Çıkış yapmak için 'exit' yazınız.");
+
+        List<int> numbers = new List<int>();
+
+        while (true) 
+        { 
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "exit")
+            {
+                break;
+            }
+
+            if(int.TryParse(input, out int number))
+            {
+                numbers.Add(number);
+            }
+        }
+        if(numbers.Count > 0)
+        {
+            int maxNumber = FindMaxNumber(numbers);
+            Console.WriteLine("Max number is : " + maxNumber);
+
+        }
+
+        //13. Soru
+
+        static string ReplaceName(string name1, string name2) 
+        {
+            string tempName = name1;
+            name1 = name2;
+
+            Console.WriteLine("İlk isim: " + name2 + "\nİkinci isim: " + tempName);
+
+            return tempName;
+        }
+        Console.WriteLine("Lütfen ilk ismi giriniz: ");
+        string name1 = Console.ReadLine();
+        Console.WriteLine("Lütfen ikinci ismi giriniz: ");
+        string name2 = Console.ReadLine();
+
+        ReplaceName(name1, name2);
+
+        //14. Soru
+
+        static bool CheckNum(int num)
+        {
+            bool check;
+            if (num%2 == 0)
+            {
+                check = true;
+            }
+            else
+            {
+                check = false;
+            }
+            Console.WriteLine("Sayınız tek ise false, çift ise true ifadesini alırsınız: " + check );
+            return check;
+        }
+
+        Console.WriteLine("Lütfen bir sayı giriniz: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        CheckNum(num);
+
+        //15. Soru
+
+        static int Distance(int speed, int time)
+        {
+            return speed * time;
+        }
+
+        Console.WriteLine("Lütfen hızınızı giriniz(m/s): ");
+        int speed = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Lütfen zamanı yazınız(s): ");
+        int time = Convert.ToInt32(Console.ReadLine());
+
+        double distance = Distance(speed, time);
+
+        Console.WriteLine("Mesafe: " + distance);
+
+        //16. Soru
+
+        // Yarıçap bilgisi verilen bir dairenin alanını hesaplayan bir metot yazınız.
+
+        static double Alan(double r)
+        {
+            int pi = 3;
+            return (r * r) * pi;
+        }
+
+        Console.WriteLine("Lütfen yarıçapı giriniz: ");
+        double r = Convert.ToDouble(Console.ReadLine());
+        double alan = Alan(r);
+
+        Console.WriteLine("Dairenin alanı:" + alan);
+
+        //17. Soru
+
+        static string Upper(string upperOne)
+        {
+            return upperOne.ToUpper();
+        }
+
+        static string Lower(string lowerOne)
+        {
+            return lowerOne.ToLower();
+        }
+
+        string origin = "Zaman bir GeRi SayIm";
+        string upperOne = Upper(origin);
+        Console.WriteLine(upperOne);
+        string lowerOne = Lower(origin);
+        Console.WriteLine(lowerOne);
+
+        //18. Soru
+
+        string wordWithSpace = "    Selamlar   ";
+
+        string newWord = wordWithSpace.Trim();
+        Console.WriteLine(newWord);
     }
+
+
 }
